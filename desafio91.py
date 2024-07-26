9d4d6eecd16e094de55a2475099ea99186395348
Alexandre <26aandrade26@gmail.com>
2024-07-26 12:49:36 +0100

    Resolução final do exercício 91

--- desafio91.py
+++ desafio91.py
@@ -1,7 +1,23 @@
-import random
+from random import randint
+from time import sleep
+from operator import itemgetter
 
-resultados = dict
+resultados = dict ()
 for i in range (1,5):
-    resultados['jogador'& i: ] = random(1,6)
+    resultados[f'Jogador {i}'] = randint(1,6)
 
-print(resultados)
+ranking = list()
+
+print("Valores sorteados: ")
+
+for k,v in resultados.items():
+    print(f"{k} tirou {v} no dado.")
+    sleep(1)
+
+ranking = sorted(resultados.items(), key=itemgetter(1),reverse=True)
+#print(ranking)
+print("-" * 30)
+print("  == RANKING DOS JOGADORES ==")
+for i, v in enumerate(ranking):
+    print(f"  {i+1}º lugar: {v[0]} com {v[1]}.")
+    sleep(1)
